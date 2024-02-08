abc704c573d47a28726027813b84f143dce6dada
EgorYakimov <151763291+EgorYakimov@users.noreply.github.com>
2024-02-08 22:50:09 +0500

    chgn BonusServiceTest

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -5,10 +5,10 @@ public class BonusServiceTest {
     @Test
     void shouldCalculateForRegisteredAndUnderLimit() {
         BonusService service = new BonusService();
-        long amound = 1000;
+        long amount = 1000;
         boolean registered = true;
         long expected = 30;
-        long actual = service.calculate(amound, registered);
+        long actual = service.calculate(amount, registered);
         Assertions.assertEquals(expected, actual);
 
     }
@@ -26,10 +26,10 @@ public class BonusServiceTest {
     @Test
     void shouldCalculateForNonRegisteredAndUnderLimit() {
         BonusService service = new BonusService();
-        long amound = 1000;
+        long amount = 1000;
         boolean registered = false;
         long expected = 10;
-        long actual = service.calculate(amound, registered);
+        long actual = service.calculate(amount, registered);
         Assertions.assertEquals(expected, actual);
     }
 
